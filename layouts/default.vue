67fc638cfac71ea3c6b8424bb0f062d672ba659d
jeon <jeon@sint.kr>
2019-12-13 17:56:21 +0900

    Dashboard shadow fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-app(light)
-    v-card.dashboard.ma-0.ma-lg-8(shaped)
+    v-card.dashboard.mx-auto.my-lg-8(shaped)
       Layout
       v-content.fill-height
         v-container(fluid).fill-height.align-start.pa-0
