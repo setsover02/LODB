7bbab9be2eacf4d851def65bd1377240b63f2391
Jeon <setsover02@naver.com>
2020-07-28 17:53:41 +0900

    FormLink move to simulation folder

--- components/simulator/character/FormLink.vue
+++ components/simulator/character/FormLink.vue
@@ -3,30 +3,86 @@ v-sheet(color="transparent")
 	v-row.px-4.py-2(align="center")
 		v-col.subtitle-2 링크
 		v-col(cols="auto").primary--text.subtitle-2 총합
-		//- 	v-chip.ml-3.white--text(small :color="totalLinkColor") {{ Math.round(getTotalLink * 100) + '%' }}
-		//- v-col(cols="auto")
-		//- 	v-btn(v-if="getTotalLink < 5" @click="SET_LINK_MAX" small text color="primary") Max
-		//- 	v-btn(v-else @click="SET_LINK_MIN" small text color="red") Min
+			v-chip.ml-3.white--text(small :color="totalLinkColor") {{ Math.round(getTotalLink * 100) + '%' }}
+		v-col(cols="auto")
+			v-btn(v-if="getTotalLink < 5" @click="SET_LINK_MAX" small text color="primary") Max
+			v-btn(v-else @click="SET_LINK_MIN" small text color="red") Min
 
 	v-row.px-4
 		//- TODO: select 5개 모두 값이 있을 경우 풀링 보너스 선택
-		v-col(v-for="(link, index) in linkSlot")
-			span {{ link.input }}
-			v-select(@change="updateLinkSlot(index, $event.target.value)" :items="linkSlotItem" solo flat dense attach append-icon="" hide-details)
+		v-col
+			v-select(v-model="linkSlot1" :items="linkSlotItem" solo flat dense attach append-icon="" suffix="%" hide-details)
+		v-col
+			v-select(v-model="linkSlot2" :items="linkSlotItem" solo flat dense attach append-icon="" suffix="%" hide-details)
+		v-col
+			v-select(v-model="linkSlot3" :items="linkSlotItem" solo flat dense attach append-icon="" suffix="%" hide-details)
+		v-col
+			v-select(v-model="linkSlot4" :items="linkSlotItem" solo flat dense attach append-icon="" suffix="%" hide-details)
+		v-col
+			v-select(v-model="linkSlot5" :items="linkSlotItem" solo flat dense attach append-icon="" suffix="%" hide-details)
 </template>
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapMutations } from "vuex";
 export default {
-	computed: {
-		linkSlot() {
-			return this.$store.state.characters.link.linkSlot;
-		},
-		...mapState("characters/link", ["linkSlotItem"]),
-	},
-	methods: {
-		updateLinkSlot(index, val) {
-			store.commit("characters/link/SET_LINK_SLOT", { index, val })
-		}
-	}
-}
+  methods: {
+    ...mapMutations("characters/link", ["SET_LINK_MAX", "SET_LINK_MIN"])
+  },
+  computed: {
+    ...mapState("characters/link", ["linkSlotItem"]),
+    ...mapGetters("characters/link", ["getTotalLink"]),
+    // 링크 슬롯(퍼센티지 합산 해야 함)
+    linkSlot1: {
+      get() {
+        return this.$store.state.characters.link.linkSlot1;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_LINK_SLOT1", value);
+      }
+    },
+    linkSlot2: {
+      get() {
+        return this.$store.state.characters.link.linkSlot2;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_LINK_SLOT2", value);
+      }
+    },
+    linkSlot3: {
+      get() {
+        return this.$store.state.characters.link.linkSlot3;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_LINK_SLOT3", value);
+      }
+    },
+    linkSlot4: {
+      get() {
+        return this.$store.state.characters.link.linkSlot4;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_LINK_SLOT4", value);
+      }
+    },
+    linkSlot5: {
+      get() {
+        return this.$store.state.characters.link.linkSlot5;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_LINK_SLOT5", value);
+      }
+    },
+    fullLinkBonus: {
+      get() {
+        return this.$store.state.enhance.fullLinkBonus;
+      },
+      set(value) {
+        this.$store.commit("characters/link/SET_FULLLINK_BONUS", value);
+      }
+    },
+    totalLinkColor() {
+      if (this.$store.getters["characters/link/getTotalLink"] < 5) return "red";
+      else return "primary";
+    }
+  }
+};
 </script>
--- store/characters/link.js
+++ store/characters/link.js
@@ -6,28 +6,55 @@ export const state = () => ({
   linkSlot3: 100,
   linkSlot4: 100,
   linkSlot5: 100,
-  fullLinkBonus: "",
-  linkSlot: [
-    {
-      input: 100
-    },
-    {
-      input: 100
-    },
-    {
-      input: 100
-    },
-    {
-      input: 100
-    },
-    {
-      input: 100
-    }
-  ]
+  fullLinkBonus: ""
 });
 
+export const getters = {
+  getTotalLink: state => {
+    return (
+      (state.linkSlot1 +
+        state.linkSlot2 +
+        state.linkSlot3 +
+        state.linkSlot4 +
+        state.linkSlot5) /
+      100
+    );
+  }
+};
+
 export const mutations = {
-  SET_LINK_SLOT(state, payload) {
-    state.linkSlot[(payload, index)].input = payload.val;
+  SET_LINK_MAX(state) {
+    (state.linkSlot1 = 100),
+      (state.linkSlot2 = 100),
+      (state.linkSlot3 = 100),
+      (state.linkSlot4 = 100),
+      (state.linkSlot5 = 100);
+  },
+  // 링크 미니멈
+  SET_LINK_MIN(state) {
+    (state.linkSlot1 = 0),
+      (state.linkSlot2 = 0),
+      (state.linkSlot3 = 0),
+      (state.linkSlot4 = 0),
+      (state.linkSlot5 = 0);
+  },
+  SET_LINK_SLOT1(state, linkSlot1) {
+    state.linkSlot1 = linkSlot1;
+  },
+  SET_LINK_SLOT2(state, linkSlot2) {
+    state.linkSlot2 = linkSlot2;
+  },
+  SET_LINK_SLOT3(state, linkSlot3) {
+    state.linkSlot3 = linkSlot3;
+  },
+  SET_LINK_SLOT4(state, linkSlot4) {
+    state.linkSlot4 = linkSlot4;
+  },
+  SET_LINK_SLOT5(state, linkSlot5) {
+    state.linkSlot5 = linkSlot5;
+  },
+  // 풀링 보너스 선택값
+  SET_FULLLINK_BONUS(state, fullLinkBonus) {
+    state.fullLinkBonus = fullLinkBonus;
   }
 };
