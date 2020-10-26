d9c3f8849bf09fa5257890c0b00b3502d5c4034f
Noah <noah@eatple.com>
2020-10-26 13:48:57 +0900

    Update AppBar.vue

--- components/default/AppBar.vue
+++ components/default/AppBar.vue
@@ -11,9 +11,9 @@
     v-spacer
     //- v-btn(icon target="blank" href="https://www.csvjson.com/csv2json" color="t100")
     //-   v-icon mdi-json
-    v-toolbar-items(v-for="(item, i) in navigation")
-      v-btn(:to="item.to" text) {{ item.title }}
-    v-spacer
+    //- v-toolbar-items(v-for="(item, i) in navigation")
+    //-   v-btn(:to="item.to" text) {{ item.title }}
+    //- v-spacer
     v-btn(color="blue" x-small text href="mailto:setsover02@naver.com") setsover02@naver.com
     v-btn.mr-1(icon target="blank" href="https://github.com/setsover02/LODB" color="t000")
       v-icon mdi-github
