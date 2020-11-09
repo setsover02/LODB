edfa6090ef396aad59f16e31310ec845110f6e80
Noah <noah@eatple.com>
2020-11-09 16:02:04 +0900

    Update 185 128

---
+++ components/doc/Timeline.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+v-sheet(color="transparent")
+	v-timeline(align-top)
+		v-timeline-item
+			v-card(color="t500")
+				v-card-title 2032
+				v-card-text.body-1 Humanity succeeded in commercializing Origin Dust
+		v-timeline-item
+			v-card(color="t500")
+				v-card-title 2049
+				v-card-text.body-1 Kim Ji Suk and Adam Johns created Saman Industry
+		v-timeline-item(v-for="i in 24")
+			div.title 2052
+			p.body-1 삼안 산업에서 바이오로이드 에바를 선보이다. 생명 윤리에 대한 논쟁이 있었으나 인간의 도구화를 막기 위해 바이오로이드는 공식적인 상품이 된다.
+</template>
---
+++ pages/doc/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+v-row.fill-height
+	v-col(cols="2") document
+	v-divider(vertical).fill-height
+	v-col(cols="8")
+		Timeline
+</template>
+<script>
+import Timeline from "~/components/doc/Timeline";
+// import Enemy from "~/components/simulator/Enemy";
+
+export default {
+	transition: "slide-y-transition",
+	components: {
+		Timeline,
+		// Enemy
+	}
+};
+</script>
